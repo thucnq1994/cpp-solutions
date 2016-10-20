07c83bccabd2be98583198747d37b03dc413d937
Toan Nguyen <toan2406@gmail.com>
2016-10-20 16:15:20 +0700

    update solution

--- basic-cpp/factorial.cpp
+++ basic-cpp/factorial.cpp
@@ -1,3 +1,7 @@
+// unsigned int Factorial( unsigned int number ) {
+//   return number > 1 ? Factorial(number-1)*number : 1;
+// }
+
 unsigned int Factorial( unsigned int number ) {
-  return number > 1 ? Factorial(number-1)*number : 1;
+    return number <= 1 ? number : Factorial(number-1)*number;
 }
